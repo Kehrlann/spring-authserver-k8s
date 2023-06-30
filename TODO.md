d690388a6624b5584d63bc120349354f146a9b99
Daniel Garnier-Moiroux <git@garnier.wf>
2023-06-30 22:37:02 +0200

    Better intro page

---
+++ TODO.md
@@ -0,0 +1,5 @@
+# TODO
+
+[ ] remove key usage entirely; most recent key is the signing key
+[ ] client == demo-client, amended with a "view token" and no messages
+[ ] server: admin page that shows you Kubectl commands (?)
--- src/main/java/wf/garnier/spring/authserverk8s/HelloController.java
+++ src/main/java/wf/garnier/spring/authserverk8s/HelloController.java
@@ -10,6 +10,16 @@ class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Bienvenue to Spring One!";
+        return """
+                <html>
+                    <head>
+                        <title>Spring Auth Server on Kubernetes</title>
+                    </head>
+                    <body>
+                    <h1>Bienvenue to Spring One!</h1>
+                    <p><a href="http://token-viewer.127.0.0.1.nip.io/">Do an OAuth2 Login!</a></p>
+                    </body>
+                </html>
+                """;
     }
 }
