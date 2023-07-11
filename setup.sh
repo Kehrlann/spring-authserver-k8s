a4210bb97ba4ed7ec97e59aa1ef6e402bbc82db3
Daniel Garnier-Moiroux <git@garnier.wf>
2023-07-11 14:08:06 +0200

    setup.sh: make idempotent, check binaries are present

--- setup.sh
+++ setup.sh
@@ -4,6 +4,32 @@ set -euo pipefail
 
 CLUSTER_NAME=spring-authserver
 
+if ! type kind 2>/dev/null; then
+  echo -e "
+!! kind is not installed, please install kind
+
+See: https://kind.sigs.k8s.io/docs/user/quick-start/#installation
+
+On macOS:
+
+  brew install kind
+"
+  exit 1
+fi
+
+if ! type kapp 2>/dev/null; then
+  echo -e "
+!! kapp is not installed, please install kapp
+
+See: https://carvel.dev/kapp/docs/v0.57.0/install/
+
+On macOS:
+
+  brew tap vmware-tanzu/carvel && brew install kapp
+"
+  exit 1
+fi
+
 if ! kind get clusters | grep "$CLUSTER_NAME"; then
   echo "~~ Setting up kind cluster"
   kind create cluster --config kind.yml --name "$CLUSTER_NAME"
@@ -16,9 +42,9 @@ kubectl rollout restart deploy/coredns -n kube-system
 echo "~~ Updating coredns to support .nip.io > done"
 
 echo "~~ Setting nginx ingress"
-kapp deploy -a nginx-ingress -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml -y
-echo "~~ Setting nginx ingress > done"
-
-echo "~~ Setting secretgen-controller"
-kapp deploy -a secretgen-controller -f https://github.com/carvel-dev/secretgen-controller/releases/latest/download/release.yml -y
-echo "~~ Setting secretgen-controller > done"
+if kapp inspect -a nginx-ingress >/dev/null; then
+  echo "~~ Setting nginx ingress > already installed, skipping"
+else
+  kapp deploy -a nginx-ingress -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml -y
+  echo "~~ Setting nginx ingress > done"
+fi
