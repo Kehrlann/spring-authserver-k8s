7f644e89c44e48310c6e9e5f5e3ca64c3b186ebe
Daniel Garnier-Moiroux <git@garnier.wf>
2023-08-02 11:39:18 +0200

    tap: use Azure as an upstream IDP

--- tap/utils/deploy-authserver.sh
+++ tap/utils/deploy-authserver.sh
@@ -7,8 +7,15 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 TAP_VALUES_FILE=$(bash "$SCRIPT_DIR/get-tap-values.sh")
 
 INGRESS_DOMAIN=$(yq ".shared.ingress_domain" "$TAP_VALUES_FILE")
+AZURE_DATA_VALUES_FLAG=""
+if [[ -f "$SCRIPT_DIR/../azure-data-values.yml" ]]; then
+  AZURE_DATA_VALUES_FLAG="--data-values-file $SCRIPT_DIR/../azure-data-values.yml"
+fi
 
-ytt --file "$SCRIPT_DIR/../authserver.yml" \
+ytt \
+  --file "$SCRIPT_DIR/../authserver-dex-idp.yml" \
+  --file "$SCRIPT_DIR/../authserver.yml" \
+  $AZURE_DATA_VALUES_FLAG \
   --data-value "domain=$INGRESS_DOMAIN" |
     kapp deploy \
       --namespace default \
